380256ffc91bc00a982181612f76b11fcd891202
mustofa binary <mustofa@regentmarkets.com>
2019-12-08 02:29:33 +0800

    put in try

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,10 +2,9 @@ const jwt = require('jsonwebtoken')
 const userModel = require('../models/user.model')
 
 const auth = async(req, res, next) => {
-    console.log(req)
-    const token = req.header('Authorization').replace('Bearer ', '')
-    const data = jwt.verify(token, process.env.JWT_KEY)
     try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const data = jwt.verify(token, process.env.JWT_KEY)
         const user = await userModel.findOne({ _id: data._id, 'tokens.token': token })
         if (!user) {
             throw new Error('No user found')
